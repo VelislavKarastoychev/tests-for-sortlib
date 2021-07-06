87414f1514bacfc2d365e741db96438bc2803099
VelislavKarastoychev <exel_mmm@abv.bg>
2021-07-06 23:47:44 +0300

    string data for testing of the bisection method.

---
+++ Tests/Data/testPutStringInSortedArray.js
@@ -0,0 +1,6 @@
+'use strict';
+const output = {};
+const scope = ['alphabet', 'banana', 'competition','crypto currency', 'dialog', 'digital', 'female', 'Yale'];
+const target = ['alphabet', 'banana', 'barbecue', 'competition','crypto currency', 'dialog', 'digital', 'female', 'Yale'];
+const argument = 'barbecue';
+module.exports = {argument, scope, target};
