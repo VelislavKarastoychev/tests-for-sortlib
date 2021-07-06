c5656964471d51827fb4490667a28f34100d2578
VelislavKarastoychev <exel_mmm@abv.bg>
2021-07-06 23:40:49 +0300

    Call all the tests and run it.

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+// run the current versions of the euriklis packages.
+const validator = require("@euriklis/validator");
+const message = require("@euriklis/message");
+const SortLib = require("@euriklis/sortlib");
+new message()
+  .set_color_yellow().bold().italic().underline()
+  .append(`This library tests the @euriklis/sortlib package.\n`)
+  .append(
+    `The current versions of the @euriklis/sortlib is ${SortLib.version}, \n`
+  )
+  .append(
+    `the current version of the @euriklis/validator is ${validator.version}\n`
+  )
+  .append(
+    `and finally the current version of the @euriklis/message is ${message.version.version}`
+  ).reset().log();
