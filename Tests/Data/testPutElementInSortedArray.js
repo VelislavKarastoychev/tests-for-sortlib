70b648fd030a96a770740ff57bfcc03cae9ebfe0
VelislavKarastoychev <exel_mmm@abv.bg>
2021-07-06 23:45:17 +0300

    data for testing the bisectional method.

---
+++ Tests/Data/testPutElementInSortedArray.js
@@ -0,0 +1,6 @@
+'use strict';
+const output = {};
+const scope = [1, 4, 5, 9, 99, 113, 422, 441, 463, 534, 536, 555, 565];
+const target = [1,3,4, 5,9, 99, 113, 422, 441, 463, 534, 536, 555, 565];
+const argument = 3;
+module.exports = output = {argument, scope, target};
