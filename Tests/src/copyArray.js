90da2ee51d452c47e96c2b3a265cfc95c872e32f
VelislavKarastoychev <exel_mmm@abv.bg>
2021-07-13 17:03:04 +0300

    Utility function that makes fast copy of an array.

---
+++ Tests/src/copyArray.js
@@ -0,0 +1,35 @@
+'use strict';
+/**
+ * 
+ * @param {Array.<number | string | object>} array
+ * @return {Array.<number | string | object>}
+ * @description this function makes fast deep copy
+ * of an array of strings, numbers or objects. We
+ * use the so called double shifting for more time
+ * efficiency.  
+ */
+function copy_array (array) {
+    const copied_array = [], n = array.length;
+    let i, j;
+    for (i = 0;i < n >> 2;i++) {
+        j = i << 2;
+        copied_array[j] = array[j];
+        ++j;
+        copied_array[j] = array[j];
+        ++j;
+        copied_array[j] = array[j];
+        ++j;
+        copied_array[j] = array[j];
+    }
+    if (n % 4 >= 1) {
+        copied_array[n - 1] = array[n - 1]; 
+    }
+    if (n % 4 >= 2) {
+        copied_array[n - 2] = array[n - 2];
+    }
+    if (n % 4 >= 3) {
+        copied_array[n - 3] = array[n - 3];
+    }
+    return copied_array;
+}
+module.exports = copy_array;
