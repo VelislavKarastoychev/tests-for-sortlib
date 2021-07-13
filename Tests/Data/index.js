770a0038af86c470ef737516210d26a4712b7c68
VelislavKarastoychev <exel_mmm@abv.bg>
2021-07-13 17:17:54 +0300

    Call all the datas

--- Tests/Data/index.js
+++ Tests/Data/index.js
@@ -1,8 +1,31 @@
 'use strict';
 const putElementInSortedArray = require('./testPutElementInSortedArray');
 const putStringInSortedArray = require('./testPutStringInSortedArray');
+const numberSort = require('./testsNumberSort');
+const stringSort = require('./testStringSort');
+const sortObjectByProperty = require('./testSortObjectByProperty');
+const sortObjectByStringProperty = require('./testSortObjectByStringProperty');
+const findBestNElements = require('./testFindBestNElements');
+const findWorstNElements = require('./testFindWorstNElements');
+const findBestNElementsInObjectArrayByProperty = require('./testFindBestNElementsInObjectArrayByProperty');
+const findWorstNElementsInObjectArrayByProperty = require('./testFindWorstNElementsInObjectArrayByProperty');
+const findBestNStringElements = require('./testFindBestNStringElements');
+const findBestNStringElementsInObjectArray = require('./testFindBestNStringElementsInObjectArray');
+const findWorstNStringElements = require('./testFindWorstNStringElements');
+const findWorstNStringElementsInObjectArray = require('./testFindWorstNStringElementsInObjectArray');
 module.exports = {
     putElementInSortedArray,
     putStringInSortedArray,
-
-}
+    numberSort,
+    stringSort,
+    sortObjectByProperty, 
+    sortObjectByStringProperty,
+    findBestNElements,
+    findWorstNElements,
+    findBestNElementsInObjectArrayByProperty,
+    findWorstNElementsInObjectArrayByProperty,
+    findBestNStringElements,
+    findBestNStringElementsInObjectArray,
+    findWorstNStringElementsInObjectArray,
+    findWorstNStringElements,
+};
