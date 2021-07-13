a56f628faa78411e6d39c242ef28db111c01e100
VelislavKarastoychev <exel_mmm@abv.bg>
2021-07-13 17:00:18 +0300

    Utility function that tests the find_best_for_object_array_by_property() method of the SortLib package.

---
+++ Tests/src/find_best_for_object_array_by_property.js
@@ -0,0 +1,37 @@
+"use strict";
+const error_message = require("./errorMessage");
+const SortLib = require("@euriklis/sortlib");
+const success_message = require("./successMessage");
+const validator = require("@euriklis/validator");
+/**
+ *
+ * @param {{scope : Array.<object>, target : Array.<object>, count : number}} data
+ * @param {'find best for object array by property for number arguments' | 'find best for object array by property with string elements'} method
+ * @description this function checks if the find_best_for_object_array_by_property
+ * works correctly for the SortLib package.
+ */
+async function find_best_for_object_array_by_property(
+  data,
+  method = "find best for object array by property for number arguments"
+) {
+  let output;
+  try {
+    new validator(
+      SortLib.find_best_for_object_array_by_property(
+        data.scope,
+        data.count
+      ).array
+    )
+      .is_same(data.target)
+      .on(true, () => {
+        output = Promise.resolve(success_message(method));
+      })
+      .on(false, () => {
+        output = Promise.reject(error_message(method));
+      });
+  } catch (error) {
+    output = Promise.reject(error);
+  }
+  return output;
+}
+module.exports = find_best_for_object_array_by_property;
