a7da23b98c6772283a36e3606e0c2f539cf9f5d0
VelislavKarastoychev <exel_mmm@abv.bg>
2021-07-13 16:56:17 +0300

    Utility finction which creates a random strings with given length of the array and the width of the symbols.

---
+++ Tests/src/generateRandomStrings.js
@@ -0,0 +1,26 @@
+"use strict";
+const SortLib = require("@euriklis/sortlib");
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+/**
+ * 
+ * @param {number} n - the number of the strings 
+ * @param {number} k - the count of the characters in the string
+ * @description this function creates random strings with equal number
+ * of characters. 
+ * @returns {Array.<string>}
+ */
+function generate_random_strings(n, k) {
+  const random_array_n = SortLib.generate_random_array(n * k);
+  // generate random strings with length of k symbols:
+  let i, j, scope = [];
+  for (i = 0; i < n; i++) {
+    scope[i] = "";
+    for (j = 0; j < k; j++) {
+      scope[i] +=
+        alphabet[(random_array_n[j * (i + 1)] * alphabet.length) >> 0];
+    }
+  }
+  return scope
+}
+
+module.exports = generate_random_strings;
