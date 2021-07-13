0a715a281428cc22f65496641f7d4312ba29e895
VelislavKarastoychev <exel_mmm@abv.bg>
2021-07-13 17:14:35 +0300

    Utility function for the add_element_in_sorted_array() method.

--- Tests/Data/testPutElementInSortedArray.js
+++ Tests/Data/testPutElementInSortedArray.js
@@ -1,6 +1,5 @@
 'use strict';
-const output = {};
-const scope = [1, 4, 5, 9, 99, 113, 422, 441, 463, 534, 536, 555, 565];
-const target = [1,3,4, 5,9, 99, 113, 422, 441, 463, 534, 536, 555, 565];
-const argument = 3;
-module.exports = output = {argument, scope, target};
+let scope = [1, 4, 5, 9, 99, 113, 422, 441, 463, 534, 536, 555, 565];
+let target = [1,3,4, 5,9, 99, 113, 422, 441, 463, 534, 536, 555, 565];
+let argument = 3;
+module.exports = {argument, scope, target};
