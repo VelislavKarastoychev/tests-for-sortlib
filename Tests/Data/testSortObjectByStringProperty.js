306b7f46485473254fb8aef4b0d3ff69d5cd5bb4
VelislavKarastoychev <exel_mmm@abv.bg>
2021-07-15 13:58:04 +0300

    data generator for the sort object array by property method with string arguments.

--- Tests/Data/testSortObjectByStringProperty.js
+++ Tests/Data/testSortObjectByStringProperty.js
@@ -1,8 +1,21 @@
 'use strict';
 const copy_array = require('../src/copyArray');
 const create_random_strings = require('../src/generateRandomStrings');
-let output;
-const scope = create_random_strings(100, 5);
-const target = copy_array(scope).sort();
-output = {scope, target};
-module.exports = output;
+const _scope_ = create_random_strings(100, 5);
+let scope = [];
+for (let i = 0;i < _scope_.length;i++) {
+    scope[i] = {
+        id : i,
+        value : _scope_[i]
+    }
+}
+let target = copy_array(scope).sort((a, b) => {
+    if (a.value > b.value) {
+        return 1;
+    }
+    if (a.value < b.value) {
+        return - 1;
+    }
+    return 0;
+});
+module.exports = {scope, target, property : 'value'};
