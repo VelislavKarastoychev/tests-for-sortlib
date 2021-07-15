5388729307001cfb595785b4da6f4b63a6a75a1c
VelislavKarastoychev <exel_mmm@abv.bg>
2021-07-15 13:52:34 +0300

    test function for success or error message for the static method add element in sorted object array by property.

---
+++ Tests/src/add_element_in_sorted_object_array_by_property.js
@@ -0,0 +1,35 @@
+'use strict';
+const data = require('../Data');
+const error_message = require('./errorMessage');
+const inverse_array = require('./inverseArray');
+const SortLib = require("@euriklis/sortlib");
+const success_message = require('./successMessage');
+/**
+ * 
+ * @param {{
+ * argument : string | number,
+ * scope : Array.<object>, 
+ * property : Array.<string> | string, 
+ * target : Array.<object>
+ * }} data 
+ * @param {'add number element in sorted object array by property' | 'add string element in sorted object array by property'} method
+ * @description this utility function tests if the static method of the SortLib
+ * package add_element_in_sorted_object_array() works correctly. We use the corresponded
+ * data in the tests.
+ */
+async function add_element_in_sorted_object_array_by_property(data, method = 'add element in sorted object array by property') {
+    let output;
+    try {
+        new validator(SortLib.add_element_in_sorted_object_array_by_property(data.scope, data.property, data.argument).array)
+            .is_same(data.target)
+            .and().bind(
+                new validator(SortLib.add_element_in_sorted_object_array_by_property(inverse_array(data.scope), data.property, data.argument).array)
+                    .is_same(inverse_array(data.target))
+            ).on(true, () => output = Promise.resolve(success_message(method)))
+            .on(false, () => output = Promise.reject(error_message(method)));
+    } catch (error) {
+        output = Promise.reject(error);
+    }
+    return output;
+}
+module.exports = add_element_in_sorted_object_array_by_property;
