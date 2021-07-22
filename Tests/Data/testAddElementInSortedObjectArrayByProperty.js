bef0014969b7215cfac0b391d9e26af89d04f70c
VelislavKarastoychev <exel_mmm@abv.bg>
2021-07-22 10:19:45 +0300

    module which generates a data set for testing of the add element in sorted object array by property method.

--- Tests/Data/testAddElementInSortedObjectArrayByProperty.js
+++ Tests/Data/testAddElementInSortedObjectArrayByProperty.js
@@ -4,19 +4,21 @@ const copy_array = require('../src/copyArray');
 /**
  * 
  * @param {number} array_length
- * @param {number} element
+ * @param {object} element
  * @returns {{scope : Array.<number>, property : string, argument : number}} 
  */
 function addElementInSortedObjectArrayByProperty(array_length, element) {
     let scope, target, copy_scope;
     scope = SortLib.generate_random_array(array_length, null, (el, i) => {
         return el = {
-            value: el,
+            attributes : {value: el},
             id: i
         }
     });
-    copy_scope = copy_array(scope).push({ value: element, id: 100 });
-    target = SortLib.sort_object_array_by_property(copy_scope, 'value', true).array
-    return { argument: element, scope, property: 'value', target }
+    copy_scope = copy_array(scope);
+    copy_scope[array_length] = element;
+    scope = SortLib.sort_object_array_by_property(scope, ['attributes', 'value'], true).array;
+    target = SortLib.sort_object_array_by_property(copy_scope, ['attributes', 'value'], true).array;
+    return { argument: element, scope, property: ['attributes', 'value'], target };
 }
 module.exports = addElementInSortedObjectArrayByProperty;
