5e33b42fad0f31ff0f0951d3a9da90369eb5e611
VelislavKarastoychev <exel_mmm@abv.bg>
2021-07-22 13:08:14 +0300

    function which generates a data set for the testing of the remove element from sorted array method

--- Tests/Data/testRemoveElementFromSortedArray.js
+++ Tests/Data/testRemoveElementFromSortedArray.js
@@ -3,14 +3,16 @@ const copy_array = require('../src/copyArray');
 const InternalError = require('../src/internalError');
 const SortLib = require('@euriklis/sortlib');
 function removeElementFromSortedArray(n, index, element) {
-    const scope = SortLib.generate_random_array(n);
+    const scope = SortLib.generate_random_array(n).sort((a, b) => a - b);
     if (typeof index !== 'number') throw InternalError(
         'test module for the remove element from sorted array method',
         'The argument index is not integer.'
     );
     if (typeof element === 'undefined') element = scope[index];
-    let i_el = scope.findIndex(element);
-    const target = [...copy_array(scope).slice(0, i_el), ...copy_array(scope).slice(i_el + 1)];
+    let i_el = scope.findIndex(el =>  el === element);
+    const target = [...copy_array(scope).slice(0, i_el), ...copy_array(scope).slice(i_el + 1)].sort((a, b) => {
+        return a - b;
+    });
     return {scope, target, element}
 }
 module.exports = removeElementFromSortedArray;
