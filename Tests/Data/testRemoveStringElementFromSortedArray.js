6d0c2eda8e43828a87fbcf4fbc6651686a9644af
VelislavKarastoychev <exel_mmm@abv.bg>
2021-07-22 13:04:25 +0300

    function that generates a data set for testing of the remove element from sorted array when the array is constructed from string elements.

---
+++ Tests/Data/testRemoveStringElementFromSortedArray.js
@@ -0,0 +1,25 @@
+'use strict';
+const internal_error = require('../src/internalError');
+const SortLib = require('@euriklis/sortlib');
+/**
+ * 
+ * @param {number} array_length 
+ * @param {number} width 
+ * @param {number} index 
+ * @param {string} element
+ * @description this function generates a data for
+ * testing of the remove element from sorted array. 
+ */
+function removeStringElementFromSortedArray (array_length, width, index, element) {
+    const scope = SortLib.generate_random_string_array(array_length, width).sort((a, b) => {
+        if (a < b) return 1;
+        if (a > b) return -1;
+        return 0;
+    });
+    if (typeof element === 'undefined') element = scope[index];
+    if (typeof index === 'undefined') internal_error('internal error in remove element from sorted array', 'the index parameter in the data generator for string elements is not defined');
+    let i_el = scope.findIndex(el => el === element);
+    const target = [...scope.slice(0, i_el), ...scope.slice(i_el + 1)];
+    return {scope, element, target};
+}
+module.exports = removeStringElementFromSortedArray;
