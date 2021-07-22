3a53c5bf258bb0fcead22a0b567e5f166ca4dcbc
VelislavKarastoychev <exel_mmm@abv.bg>
2021-07-22 09:24:09 +0300

    test function for the remove element from sorted array static method

---
+++ Tests/src/remove_element_from_sorted_array.js
@@ -0,0 +1,11 @@
+'use strict';
+const error_message = require('./errorMessage');
+const SortLib = require('@euriklis/sortlib');
+const success_message = require('./successMessage');
+const validator = require('@euriklis/validator');
+function remove_element_from_sorted_array (data, method) {
+    let output;
+    let result = SortLib;
+    return output;
+}
+module.exports = remove_element_from_sorted_array; 
