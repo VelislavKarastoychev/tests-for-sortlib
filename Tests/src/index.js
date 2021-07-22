9e76666f538fcb733008cc0e0820da068777a645
VelislavKarastoychev <exel_mmm@abv.bg>
2021-07-22 10:26:49 +0300

    Collect all testing methods.

--- Tests/src/index.js
+++ Tests/src/index.js
@@ -1,24 +1,31 @@
 'use strict';
+const add_element_in_sorted_object_array_by_property = require('./add_element_in_sorted_object_array_by_property');
 const bubble_sort = require('./bubble_sort');
 const bucket_sort = require('./bucket_sort');
 const cocktail_sort = require('./cocktail_sort');
 const find_best_elements = require('./find_best_elements');
 const find_best_for_object_array_by_property = require('./find_best_for_object_array_by_property');
+const find_elements_in_sorted_array = require('./find_elements_in_sorted_array');
+const find_elements_in_sorted_object_array_by_property = require('./find_elements_in_sorted_object_array_by_property');
 const find_worst_elements = require('./find_worst_elements');
 const find_worst_for_object_array_by_property = require('./find_worst_for_object_array_by_property');
+const heap_sort = require('./heap_sort');
 const insertion_sort = require('./insertion_sort');
 const merge_sort = require('./merge_sort');
 const putElementInSortedArray = require('./putElementInSortedArray');
 const quick_sort = require('./quick_sort');
+const remove_element_from_sorted_array = require('./remove_element_from_sorted_array');
 const selection_sort = require('./selection_sort');
-const heap_sort = require('./heap_sort');
 const sort_object_array_by_property = require('./sort_object_array_by_property');
 module.exports = {
+    add_element_in_sorted_object_array_by_property,
     bubble_sort,
     bucket_sort,
     cocktail_sort,
     find_best_elements,
     find_best_for_object_array_by_property,
+    find_elements_in_sorted_array,
+    find_elements_in_sorted_object_array_by_property,
     find_worst_elements,
     find_worst_for_object_array_by_property,
     heap_sort,
@@ -26,6 +33,7 @@ module.exports = {
     merge_sort,
     putElementInSortedArray,
     quick_sort,
+    remove_element_from_sorted_array,
     selection_sort,
     sort_object_array_by_property,
 };
