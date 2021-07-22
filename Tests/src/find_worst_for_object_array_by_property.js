c51f5b2390a9f24d6915d0bdd7a2869e5bbf950c
VelislavKarastoychev <exel_mmm@abv.bg>
2021-07-22 10:27:32 +0300

    test module for the find worst for object array by property method.

--- Tests/src/find_worst_for_object_array_by_property.js
+++ Tests/src/find_worst_for_object_array_by_property.js
@@ -5,7 +5,7 @@ const success_message = require("./successMessage");
 const validator = require("@euriklis/validator");
 /**
  *
- * @param {{scope : Array.<object>, target : Array.<object>, count : number}} data
+ * @param {{scope : Array.<object>, property: string, target : Array.<object>, count : number}} data
  * @param {'find worst for object array by property' | 'find worst for object array by property for string elements'} method
  * @description this function checks if the method of the SortLib
  * package find_worst_for_object_array_by_property works correctly.
@@ -14,14 +14,14 @@ async function find_worst_for_object_array_by_property(
   data,
   method = "find worst for object array by property"
 ) {
-  let output;
+  let output, result;
   try {
-    new validator(
-      SortLib.find_worst_for_object_array_by_property(
-        data.scope,
-        data.count
-      ).array
-    )
+    result = SortLib.find_worst_for_object_array_by_property(
+      data.scope,
+      data.property,
+      data.count,
+    ).array
+    new validator(result)
       .is_same(data.target)
       .on(true, () => {
         output = Promise.resolve(success_message(method));
