d2f76e6009baea238c130b66fa55704ba594b9fe
VelislavKarastoychev <exel_mmm@abv.bg>
2021-07-22 10:29:47 +0300

    test method for the add element in sorted object array by property method.

--- Tests/src/add_element_in_sorted_object_array_by_property.js
+++ Tests/src/add_element_in_sorted_object_array_by_property.js
@@ -4,6 +4,7 @@ const error_message = require('./errorMessage');
 const inverse_array = require('./inverseArray');
 const SortLib = require("@euriklis/sortlib");
 const success_message = require('./successMessage');
+const validator = require('@euriklis/validator');
 /**
  * 
  * @param {{
@@ -20,13 +21,15 @@ const success_message = require('./successMessage');
 async function add_element_in_sorted_object_array_by_property(data, method = 'add element in sorted object array by property') {
     let output;
     try {
-        new validator(SortLib.add_element_in_sorted_object_array_by_property(data.scope, data.property, data.argument).array)
+        new validator(SortLib.add_element_in_sorted_object_array_by_property(data.scope, data.property, data.argument))
             .is_same(data.target)
             .and().bind(
-                new validator(SortLib.add_element_in_sorted_object_array_by_property(inverse_array(data.scope), data.property, data.argument).array)
+                new validator(SortLib.add_element_in_sorted_object_array_by_property(inverse_array(data.scope), data.property, data.argument, false))
                     .is_same(inverse_array(data.target))
             ).on(true, () => output = Promise.resolve(success_message(method)))
-            .on(false, () => output = Promise.reject(error_message(method)));
+            .on(false, () => {
+                output = Promise.reject(error_message(method))
+            });
     } catch (error) {
         output = Promise.reject(error);
     }
