2c0e6d0cb52a22564de50c820700b8fb2c6332a4
VelislavKarastoychev <exel_mmm@abv.bg>
2021-07-23 19:19:15 +0300

    Test function fro the removing element from sorted array.

--- Tests/src/remove_string_element_from_sorted_array.js
+++ Tests/src/remove_string_element_from_sorted_array.js
@@ -7,7 +7,6 @@ const unexpected_error_message = require('./unexpectedErrorMessage');
 function remove_string_element_from_sorted_array(data, method) {
     let output, result;
     try {
-        console.log(data.element)
         result = SortLib.remove_element_from_sorted_array(data.scope, data.element).array;
         new validator(result).is_same(data.target)
             .on(true, () => output = Promise.resolve(success_message(method)))
