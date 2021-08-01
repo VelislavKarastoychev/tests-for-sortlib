62985e375529b215deb9061353ca00800e148eba
VelislavKarastoychev <exel_mmm@abv.bg>
2021-08-02 02:00:35 +0300

    Additional test that ensures that the find in sorted array works correctly for incomplete insertion of the elements.

---
+++ Tests/src/additional_test_for_find_in_sorted_array.js
@@ -0,0 +1,46 @@
+'use strict';
+const error_message = require('./errorMessage');
+const copy_array = require('./copyArray');
+const SortLib = require('@euriklis/sortlib');
+const success_message = require('./successMessage');
+const validator = require('@euriklis/validator');
+/**
+ * 
+ * @param {'additional test for find in sorted arrays'} method
+ * @description this is an additional method for testing of the find in sorted array.
+ * It is well known that this method works when we will put the element with all the
+ * properties of the array, but if we insert only the interested properties then we do
+ * not know if the method will work correctly. So we want to test this method for incomplete
+ * object structure of the element of an object array of elements.
+ */
+async function additional_test_for_find_in_sorted_arrays(method = 'additional test for find in sorted object arrays') {
+    let output;
+    // create an object array.
+    const scope = SortLib.generate_random_array(100, null, (el, i) => {
+        let usernames_list = ['Pentium', 'IBM', 'Microsoft', 'Mac'];
+        return {
+            _id: i + 1,
+            process_power: el,
+            username: usernames_list[(Math.random() * 4) >> 0],
+            architecture: {
+                cores: (Math.random() * 100) >> 0,
+                value: Math.random()
+            }
+        };
+    }).sort((a, b) => {
+        return a.architecture.cores - b.architecture.cores;
+    });
+    const target = copy_array(scope).filter(item => {
+        return item.architecture.cores === 40;
+    });
+    try {
+        new validator(SortLib.find_elements_in_sorted_object_array_by_property(scope, ['architecture', 'cores'], { architecture: { cores: 40 } }).array)
+            .is_same(target).on(true, () => {
+                output = Promise.resolve(success_message(method));
+            }).on(false, () => output = Promise.reject(method));
+    } catch (error) {
+        output = Promise.reject(error);
+    }
+    return output;
+}
+module.exports = additional_test_for_find_in_sorted_arrays;
