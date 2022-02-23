20f6dd2fb3f6bc2b099bea83db70039f0fe5292e
VelislavKarastoychev <exel_mmm@abv.bg>
2022-02-23 23:51:27 +0200

    test of the remove element from sorted array

--- Tests/src/remove_element_from_sorted_array.js
+++ Tests/src/remove_element_from_sorted_array.js
@@ -1,9 +1,9 @@
 'use strict';
-const error_message = require('./errorMessage');
-const SortLib = require('@euriklis/sortlib');
-const success_message = require('./successMessage');
-const validator = require('@euriklis/validator');
-const unexpected_error_message = require('./unexpectedErrorMessage');
+import error_message from './errorMessage.js';
+import SortLib from '@euriklis/sortlib';
+import success_message from './successMessage.js';
+import validator from '@euriklis/validator';
+import unexpected_error_message from './unexpectedErrorMessage.js';
 /**
  * 
  * @param {{scope: Array.<number | string>, element: number | string, target: Array.<string | number>}} data 
@@ -24,4 +24,4 @@ function remove_element_from_sorted_array(data, method) {
     }
     return output;
 }
-module.exports = remove_element_from_sorted_array; 
+export default remove_element_from_sorted_array; 
