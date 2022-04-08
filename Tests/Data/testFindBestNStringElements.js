c52d6d990834da26329a9b51b8e781d644b1b3d7
VelislavKarastoychev <exel_mmm@abv.bg>
2022-04-08 14:48:23 +0300

    Implementation of the find best elements testser for string array elements.

--- Tests/Data/testFindBestNStringElements.js
+++ Tests/Data/testFindBestNStringElements.js
@@ -1,7 +1,7 @@
 'use strict';
-const create_random_string = require('../src/generateRandomStrings');
-const copy_array = require('../src/copyArray');
-const invert_array = require('../src/inverseArray');
+import create_random_string from '../src/generateRandomStrings.js';
+import copy_array from '../src/copyArray.js';
+import invert_array from '../src/inverseArray.js';
 /**
  * 
  * @param {number} n - the length of the string array. 
@@ -9,9 +9,9 @@ const invert_array = require('../src/inverseArray');
  * @param {number} k - the count of the best string elements that will be return.
  * @returns {{scope:Array.<string>, target : Array.<string>}} 
  */
-function findBestNStringElements (n, k, t) {
+function findBestNStringElements(n, k, t) {
     let scope = create_random_string(n, t);
     let target = invert_array(copy_array(scope).sort()).slice(0, k);
-    return {scope, target, count : k};
+    return { scope, target, count: k };
 }
-module.exports = findBestNStringElements;
+export default findBestNStringElements;
