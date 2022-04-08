f2fb2a91522835325e7814e577f18580a411092d
VelislavKarastoychev <exel_mmm@abv.bg>
2022-04-08 14:43:21 +0300

    implementation of the success message.

--- Tests/src/successMessage.js
+++ Tests/src/successMessage.js
@@ -1,16 +1,17 @@
 "use strict";
-const message = require("@euriklis/message");
+import message from "@euriklis/message";
+
 function success_message(text) {
-  return new message()
-    .bold()
-    .italic()
-    .set_color_green()
-    .append_check_mark()
-    .append_white_space()
-    .reset()
-    .set_color_blue()
-    .append(`The ${text} was tested successfully.`)
-    .reset()
-    .log();
+    return new message()
+        .bold()
+        .italic()
+        .set_color_green()
+        .append_check_mark()
+        .append_white_space()
+        .reset()
+        .set_color_blue()
+        .append(`The ${text} was tested successfully.`)
+        .reset()
+        .log();
 }
-module.exports = success_message;
+export default success_message;
