eacff39b940acc838c283a39fcf4a4cee3b50c24
VelislavKarastoychev <exel_mmm@abv.bg>
2022-04-08 14:42:02 +0300

    unexpected error message implementation.

--- Tests/src/unexpectedErrorMessage.js
+++ Tests/src/unexpectedErrorMessage.js
@@ -1,24 +1,24 @@
 "use strict";
-const message = require("@euriklis/message");
-module.exports = (err_message) => {
-  const err_name = new message()
-    .bold()
-    .italic()
-    .underline()
-    .set_color_yellow()
-    .append("Unexpected error in the test of @euriklis/sortlib package")
-    .reset().text;
-  const styled_err_message = new message()
-    .bold()
-    .italic()
-    .underscore()
-    .set_color_cyan()
-    .append(err_message)
-    .reset().text;
-  return new message()
-    .append(err_name)
-    .append("\n")
-    .append(styled_err_message)
-    .reset()
-    .log();
+import message from "@euriklis/message";
+export default (err_message) => {
+    const err_name = new message()
+        .bold()
+        .italic()
+        .underline()
+        .set_color_yellow()
+        .append("Unexpected error in the test of @euriklis/sortlib package")
+        .reset().text;
+    const styled_err_message = new message()
+        .bold()
+        .italic()
+        .underscore()
+        .set_color_cyan()
+        .append(err_message)
+        .reset().text;
+    return new message()
+        .append(err_name)
+        .append("\n")
+        .append(styled_err_message)
+        .reset()
+        .log();
 };
