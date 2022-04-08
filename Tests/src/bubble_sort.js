ece3127ef963e8bc3118d826edf40d273591d417
VelislavKarastoychev <exel_mmm@abv.bg>
2022-04-08 14:45:41 +0300

    Implementation of the testing procefure for the bubble sort algorithm.

--- Tests/src/bubble_sort.js
+++ Tests/src/bubble_sort.js
@@ -15,15 +15,15 @@ import validator from '@euriklis/validator';
  * if the tests is not correct and a success message if
  * the results are consistent with the experimental.
  */
-async function bubble_sort (data, method) {
+async function bubble_sort(data, method) {
     let output;
     try {
-    new validator(SortLib.bubble_sort(data.scope, true).array)
-    .is_same(data.target).and().bind(
-        new validator(SortLib.bubble_sort(data.scope, false).array)
-        .is_same(inverse_array(data.target))
-    ).on(true, () => output = Promise.resolve(success_message(method)))
-    .on(false, () => output = Promise.reject(error_message(method)));
+        new validator(SortLib.bubble_sort(data.scope, true).array)
+            .is_same(data.target).and().bind(
+                new validator(SortLib.bubble_sort(data.scope, false).array)
+                .is_same(inverse_array(data.target))
+            ).on(true, () => output = Promise.resolve(success_message(method)))
+            .on(false, () => output = Promise.reject(error_message(method)));
     } catch (error) {
         output = Promise.reject(error);
     }
