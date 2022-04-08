8968e6e985d9211aa3319aa2a33ab4a191d2833a
VelislavKarastoychev <exel_mmm@abv.bg>
2022-04-08 14:41:22 +0300

    starting the test processing.

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ import message from '@euriklis/message';
 import SortLib from '@euriklis/sortlib';
 import tests from './Tests/index.js';
 new message()
-  .set_color_yellow().bold().italic().underline()
-  .append(`This library tests the @euriklis/sortlib package.\n`)
-  .append(
-    `The current versions of the @euriklis/sortlib is ${SortLib.version}, \n`
-  )
-  .append(
-    `the current version of the @euriklis/validator is ${validator.version}\n`
-  )
-  .append(
-    `and finally the current version of the @euriklis/message is ${message.version.version}`
-  ).reset().log();
+    .set_color_yellow().bold().italic().underline()
+    .append(`This library tests the @euriklis/sortlib package.\n`)
+    .append(
+        `The current versions of the @euriklis/sortlib is ${SortLib.version}, \n`
+    )
+    .append(
+        `the current version of the @euriklis/validator is ${validator.version}\n`
+    )
+    .append(
+        `and finally the current version of the @euriklis/message is ${message.version}`
+    ).reset().log();
 tests();
